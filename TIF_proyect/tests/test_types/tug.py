0c8b91fbd977af0449c40109508b76328ee5f6b7
Lucio Sepulveda <lucio.sepulveda.12@gmail.com>
2024-02-27 10:42:19 -0300

    avance con el test tug
    Ya procesa el video que se le envia y lo guarda en el mismo lugar con el nombre, agregando resultado.
    Falta hacer los calculos

--- TIF_proyect/tests/test_types/tug.py
+++ TIF_proyect/tests/test_types/tug.py
@@ -5,13 +5,20 @@ Created on Sat Feb 24 13:45:16 2024
 @author: Usuario
 """
 
-from test_types.test_base import TestBase
+from test_base import TestBase
 import mediapipe as mp
 import cv2
+import numpy as np
 
 class TUG(TestBase):
     def __init__(self):
         super().__init__("Time Up and Go - TUG")
+        
+        # VARIBALES NECESARIAS PARA SABER SI EL VIDEO ES PROCESABLE
+        self.skipped_frame_count = 0
+        self.accumulated_error = 0
+        self.previous_skipped_frame = 0
+        self.MAX_ACCUMULATE_ERROR = 10
     
     def apply_test(self, video, video_path):
         # Implementación específica del test TUG
@@ -31,12 +38,13 @@ class TUG(TestBase):
     
     """
     PROCESA EL VIDEO CON MEDIAPIPE
-    DEVUELVE EL VIDEO PROCESADO Y EL RESULTADO DE MEDIAPIPE PARA REALIZAR
-    LOS CALCULOS NECESARIOS
+    REPRESENTA EL WHILE QUE PROCESA CADA FRAME
     """
     def process_video(self, video, video_path):
         mp_drawing = mp.solutions.drawing_utils
         mp_pose = mp.solutions.pose
+        
+        frame_count = 0
         
         video_result = self.create_video(video, video_path)
         
@@ -47,8 +55,32 @@ class TUG(TestBase):
             ret, frame = video.read()
             
             while ret :
+                # PROCESO EL FRAME
                 frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 result = pose.process(frame_rgb)
+                
+                # COMPRUEBO EL RESULTADO
+                if not self.is_good_result(result, frame_count):
+                    # logica para descartar el video
+                    pass
+                
+                # OBTENGO LOS LANDMARKS
+                width  = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+                height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                landmarks_x, landmarks_y = self.get_landmarks(result, width, height)
+                
+                # CALCULO LOS ANGULOS
+                self.calculate_angles(landmarks_x, landmarks_y)
+                
+                # GENERO EL FRAME OUTPUT
+                output_frame = self.generate_output_frame(frame, landmarks_x, landmarks_y)
+                
+                # GUARDO EL FRAME OUTPUT
+                video_result.write(output_frame)
+                
+                # PASO AL SIGUIENTE FRAME
+                ret, frame = video.read()
+                frame_count += 1
         
 
     def create_video(self, video, video_path):
@@ -57,17 +89,89 @@ class TUG(TestBase):
         FPS_result = video.get(cv2.CAP_PROP_FPS)
         
         # resolucion
-        width_original  = video.get(cv2.CAP_PROP_FRAME_WIDTH)   # float "width"
-        height_original = video.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float "height"
+        width  = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))   # float "width"
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float "height"
         
-        scale_percent = 700 * 100 / height_original # Porcentaje de escalado para el video a guardar (será el mismo para el video original a mostrar)
-        width_result = int(width_original * scale_percent / 100)
-        height_result = int(height_original * scale_percent / 100)
-        resolution_result = (width_result, height_result)   #ej. (640, 480)
+        resolution = (width, height)   #ej. (640, 480)
         
-        video_result = cv2.VideoWriter(video_file_result, fourcc, FPS_result, resolution_result) # (name.mp4, fourcc, FPS, resolution)
+        video_result = cv2.VideoWriter(video_file_result, fourcc, FPS_result, resolution) # (name.mp4, fourcc, FPS, resolution)
         return video_result
+    
+    
+    def get_landmarks(self, result, width, height):
+        landmarks_x = []
+        landmarks_y = []
+        
+        if result.pose_landmarks is not None:
+            # escribo los landsmarks que interesan para el proyecto
+            # pierna derecha --> 24 , 26 , 28
+            # pierna izquierda --> 23 , 25 , 27
+            index = [ 24 , 26 , 28 , 23 , 25 , 27 ]
+            
+            # tomo los landmarks
+            for i in index:
+                x = int( result.pose_landmarks.landmark[i].x * width )
+                y = int( result.pose_landmarks.landmark[i].y * height )
+                
+                landmarks_x.append(x)
+                landmarks_y.append(y)
+        
+        return landmarks_x , landmarks_y
+    
+    
+    def calculate_angles(self, landmarks_x, landmarks_y):
+        pass
+    
+    
+    def generate_output_frame(self, frame, landmarks_x, landmarks_y):
+        # GENERO LAS LINEAS
+        aux_image = np.zeros(frame.shape, np.uint8)
+        #Rigth leg
+        cv2.line(aux_image, (landmarks_x[0], landmarks_y[0]), (landmarks_x[1], landmarks_y[1]), (3, 202, 251), 20)
+        cv2.line(aux_image, (landmarks_x[1], landmarks_y[1]), (landmarks_x[2], landmarks_y[2]), (3, 202, 251), 20)
+        #Left leg
+        cv2.line(aux_image, (landmarks_x[3], landmarks_y[3]), (landmarks_x[4], landmarks_y[4]), (3, 202, 251), 20)
+        cv2.line(aux_image, (landmarks_x[4], landmarks_y[4]), (landmarks_x[5], landmarks_y[5]), (3, 202, 251), 20)
+        
+        # AGREGO LAS LINEAS
+        output = cv2.addWeighted(frame, 1, aux_image, 0.8, 0)
+        
+        # AGREGO LOS CIRCULOS
+        cv2.circle(output, (landmarks_x[0], landmarks_y[0]), 6, (5,5,170), 4)
+        cv2.circle(output, (landmarks_x[1], landmarks_y[1]), 6, (5,5,170), 4)
+        cv2.circle(output, (landmarks_x[2], landmarks_y[2]), 6, (5,5,170), 4)
+
+        cv2.circle(output, (landmarks_x[3], landmarks_y[3]), 6, (5,5,170), 4)
+        cv2.circle(output, (landmarks_x[4], landmarks_y[4]), 6, (5,5,170), 4)
+        cv2.circle(output, (landmarks_x[5], landmarks_y[5]), 6, (5,5,170), 4)
+        
+        return output
+        
+    
+    
+    def is_good_result(self, result, frame_count):
+        
+        if result.pose_landmarks is None:
+            self.skipped_frame_count += 1
+            
+            # LOGICA PARA EL FRAME ANTERIOR
+            if self.previous_skipped_frame == frame_count - 1:
+                self.accumulated_error += 1
+            else:
+                self.accumulated_error = 0
+            
+            self.previous_skipped_frame = frame_count
+            
+            # LOGICA PARA DESCARTAR EL VIDEO
+        
+        return self.accumulated_error == self.MAX_ACCUMULATE_ERROR
+                
+                
         
 
 if __name__ == "__main__":
-    pass
+    tug = TUG()
+    video_path = "D:/repositorio/TIF_repo/TIF/TIF_proyect/tests/test_types/TUG2.mp4"
+    video = cv2.VideoCapture(video_path)
+    
+    tug.process_video(video, video_path)
